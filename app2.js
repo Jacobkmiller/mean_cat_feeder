ca0a367d492d5850421798cc8beb572c4e1dd334
Zak Espley <zespley@gmail.com>
2017-05-24 21:14:50 -0700

    Copied the index.html file from the h624 player. Copied the server-rpi file to app2.js. Now to run use node app2.js

---
+++ app2.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Zachary on 5/24/2017.
+ */
+"use strict";
+
+/**
+* Run this on a raspberry pi
+* then browse (using google chrome/firefox) to http://[pi ip]:8080/
+*/
+
+
+const http    = require('http');
+const express = require('express');
+
+
+const WebStreamerServer = require('./h264-live-player/lib/raspivid');
+
+const app  = express();
+
+  //public website
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname ));
+app.use(express.static(__dirname + '/vendor/dist'));
+
+const server  = http.createServer(app);
+const silence = new WebStreamerServer(server);
+
+const port = 8081;
+console.log("Listening on port " + port);
+server.listen(port);
+
